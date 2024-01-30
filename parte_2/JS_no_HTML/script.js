76e1b9c3419c831ff0906f36f744cca910c4532c
RamonSFR <ramonsaviofontesrocha@gmail.com>
2024-01-30 15:06:05 -0300

    finalização

--- parte_2/JS_no_HTML/script.js
+++ parte_2/JS_no_HTML/script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form-deposito');
+const nomeBeneficiario = document.getElementById('nome-beneficiario');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
@@ -9,18 +10,33 @@ form.addEventListener('submit', (e) => {
     let formEValido = false;
     e.preventDefault();
 
-    const nomeBeneficiario = document.getElementById('nome-beneficiario');
     const numeroContaBeneficiario = document.getElementById('numero-conta');
     const valorDeposito = document.getElementById('valor-deposito');
-    const mensagemSucesso = `Montante de: ${valorDeposito.value} depositado para o cliente ${nomeBeneficiario.value} - conta ${numeroContaBeneficiario.value}`;
+    const mensagemSucesso = `Montante de: <b>${valorDeposito.value}</b> depositado para o cliente <b>${nomeBeneficiario.value}</b> - conta <b>${numeroContaBeneficiario.value}</b>`;
 
     formEValido = validaNome(nomeBeneficiario.value);
     if (formEValido) {
-        alert(mensagemSucesso);
+        const containerMensagemSucesso = document.querySelector(".success-message");
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
         nomeBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
         numeroContaBeneficiario.value = '';
     } else {
-        alert("O nome não está completo");
+        document.querySelector(".error-message").style.display = 'block';
+        nomeBeneficiario.style.border = "1px solid Red";
+    }
+})
+
+nomeBeneficiario.addEventListener("keyup", (e) => {
+    formEValido = validaNome(e.target.value);
+
+    if (!formEValido) {
+        nomeBeneficiario.classList.add("error");
+        document.querySelector('.error-message').style.display = "block";
+    } else {
+        nomeBeneficiario.classList.remove("error");
+        document.querySelector('.error-message').style.display = "none";
     }
 })
