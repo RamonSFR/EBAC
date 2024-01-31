082165068b53e1bdbea7445e57475e7cfa5c91e1
RamonSFR <ramonsaviofontesrocha@gmail.com>
2024-01-30 21:20:01 -0300

    organização

--- parte_2/projeto_2/script.js
+++ parte_2/projeto_2/script.js
@@ -1,11 +1,17 @@
 const form = document.querySelector('#form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando">'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado">'
+
 let linhas = '';
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
     const inputNomeAtividade = document.querySelector('#nome-atividade');
     const inputNotaAtividade = document.querySelector('#nota-atividade');
 
@@ -17,9 +23,11 @@ form.addEventListener('submit', (e) => {
 
     linhas += linha;
 
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
-})
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
