aa262309e8e17d4d4697d6da88411e6b42965d55
RamonSFR <ramonsaviofontesrocha@gmail.com>
2024-02-01 16:57:40 -0300

    base introdução JQuery

---
+++ parte_2/JQuery/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('header button').click(function () {
+        $('form').slideDown();
+    })
+
+    $('#btn-cancelar').click(function () {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+    })
+})
