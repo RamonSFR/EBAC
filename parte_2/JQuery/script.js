447cadd3ff6120622d3f2478de773c30fc7372d0
RamonSFR <ramonsaviofontesrocha@gmail.com>
2024-02-01 17:17:00 -0300

    finalização

--- parte_2/JQuery/script.js
+++ parte_2/JQuery/script.js
@@ -9,5 +9,18 @@ $(document).ready(function () {
 
     $('form').on('submit', function (e) {
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display:none;"></li>')
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" title="Ver Imagem em tamanho real" target="_blank">
+                    Ver Imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(750);
+        $('#endereco-imagem-nova').val('')
     })
 })
