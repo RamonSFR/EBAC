454455c60a40f579f91863f1dcdb05ae63762fcd
RamonSFR <ramonsaviofontesrocha@gmail.com>
2024-02-05 13:34:54 -0300

    validate

--- parte_2/plugins_JQuery/js/script.js
+++ parte_2/plugins_JQuery/js/script.js
@@ -7,4 +7,35 @@ $(document).ready(() => {
 
     $('#telefone').mask('(00) 00000-0000')
     $('#cpf').mask('000.000.000-00')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true
+            },
+            messages: {
+                nome: 'Por favor, insira seu nome'
+            },
+            submitHandler: function (form) {
+                console.log(form)
+            },
+            invalidHandler: (evento, validador) => {
+                let camposIncorretos = validador.numberOfInvalids();
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    })
 });
