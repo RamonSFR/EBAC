33e634f809af9a6bebd0b07179b26e9454013db3
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-03-14 18:28:48 -0300

    funcao default

--- parte_4/grunt/gruntfile.js
+++ parte_4/grunt/gruntfile.js
@@ -2,4 +2,18 @@ module.exports = (grunt) => {
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
     })
+
+    grunt.registerTask('olaGrunt', function() {
+        const done = this.async();
+        setTimeout(() => {
+            console.log("Olá grunt");
+            done();
+        }, 3000);
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', [
+        'olaGrunt'
+    ]);
 }
