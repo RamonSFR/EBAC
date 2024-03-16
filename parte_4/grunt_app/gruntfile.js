1f3f96aefb87adf27bf073821495b7aeb7cd6889
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-03-16 16:13:13 -0300

    js import fix

--- parte_4/grunt_app/gruntfile.js
+++ parte_4/grunt_app/gruntfile.js
@@ -59,6 +59,10 @@ module.exports = function (grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
                         }
                     ]
                 },
