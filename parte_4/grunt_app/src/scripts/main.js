8cbb90e29a7f76e84166b32f93e121ecf4cbde57
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-03-16 15:31:40 -0300

    a

--- parte_4/grunt_app/src/scripts/main.js
+++ parte_4/grunt_app/src/scripts/main.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
         let numeroMax = document.getElementById('numero-max').value;
         numeroMax = parseInt(numeroMax);
 
-        let numeroAleatorio = Math.random() * numeroMax;
-        alert(numeroAleatorio);
+        let numeroAleatorio = Math.round((Math.random() * numeroMax));
+
+        const textoResultado = `O número sorteado foi: <span id="valor_resultado">${numeroAleatorio}</span>`;
+        document.querySelector('.resultado').innerHTML = textoResultado;
     })
 })
