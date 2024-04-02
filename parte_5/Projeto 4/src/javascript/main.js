6cee58b321af3fa0d8d2edbf9528f6d742005f6e
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-02 11:25:31 -0300

    faq

--- parte_5/Projeto 4/src/javascript/main.js
+++ parte_5/Projeto 4/src/javascript/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', (botao) => {
@@ -8,6 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
             escondeAbas();
             aba.classList.add('shows__list--is-active');
             botao.target.classList.add('shows__tabs__button--is-active');
+        })
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', (elemento) => {
+            const classe = "faq__questions__item--is-open";
+            elemento.target.parentNode.classList.toggle(classe);
         })
     }
 })
