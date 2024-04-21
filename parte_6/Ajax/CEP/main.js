cc68d8bd668d13e8218750726cf596ebdf515b5e
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-04-21 16:19:41 -0300

    fetch api

--- parte_6/Ajax/CEP/main.js
+++ parte_6/Ajax/CEP/main.js
@@ -20,17 +20,36 @@ $(document).ready(() => {
 
     $('#btn-buscar-cep').click(() => {
         const cep = $('#cep').val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json/resultado`;
         const botao = $('#btn-buscar-cep');
         
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done((resposta) => {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
+        // $.ajax(endpoint).done((resposta) => {
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro} - ${bairro} - ${cidade} - ${estado}`;
+        //     $('#endereco').val(endereco);
+
+        //     setTimeout(() => {
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 2000);
+        // })
+
+        // Fetch API
+
+        fetch(endpoint).then((resposta) => {
+            return repoosta.json();
+        })
+        .then((json) => {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
             const endereco = `${logradouro} - ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco);
 
