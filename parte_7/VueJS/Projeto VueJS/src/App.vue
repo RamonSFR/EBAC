aae65d22839a306f5debdeb93ee6ded29b2d340b
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-05-05 16:12:58 -0300

    aula 1

--- parte_7/VueJS/Projeto VueJS/src/App.vue
+++ parte_7/VueJS/Projeto VueJS/src/App.vue
@@ -1,16 +1,24 @@
 <script setup>
 const nome = "Ramon";
-document.querySelector('h1').innerHTML = nome; // JS Padrão
+const meuObj = {
+  nome: "Ramon",
+  idade: 19,
+  filmeFavorito: "Clube da Luta",
+}
+
+function dizOi(nome) {
+  return `${nome} diz oi`;
+}
+
+const imgBatman = 'https://files.tecnoblog.net/wp-content/uploads/2021/04/Qual-a-ordem-cronologica-dos-filmes-do-Batman-Deny-Freeman-Flickr.jpg'
+const botaoEstaDesabilitado = false;
 </script>
 
 <template>
-  <header>
-    <h1>Olá</h1>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{meuObj.filmeFavorito}}</h1>
+  <h2>{{dizOi('Ramon') }}</h2> <!--Mustashes-->
+  <img v-bind:src="imgBatman" alt="Imagem do Batman"> <!--Binds-->
+  <button :disabled="!botaoEstaDesabilitado">Enviar Mensagem</button>
 </template>
 
 <style scoped>
