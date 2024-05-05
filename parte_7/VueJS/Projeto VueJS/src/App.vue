a50ffa7ff847371b7baa6b45a459c5563a1825da
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-05-05 15:59:46 -0300

    setup vue

---
+++ parte_7/VueJS/Projeto VueJS/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const nome = "Ramon";
+document.querySelector('h1').innerHTML = nome; // JS Padrão
+</script>
+
+<template>
+  <header>
+    <h1>Olá</h1>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
