dc27d3ae4a9f14fc144e5f401d2283851044338c
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-05-05 16:22:58 -0300

    v-if e v-show

--- parte_7/VueJS/Projeto VueJS/src/App.vue
+++ parte_7/VueJS/Projeto VueJS/src/App.vue
@@ -11,16 +11,26 @@ function dizOi(nome) {
 }
 
 const imgBatman = 'https://files.tecnoblog.net/wp-content/uploads/2021/04/Qual-a-ordem-cronologica-dos-filmes-do-Batman-Deny-Freeman-Flickr.jpg'
+const imgSuperman = 'https://rollingstone.uol.com.br/media/uploads/2024/03/james-gunn-revela-primeira-imagem-e-anuncia-novo-titulo-de-superman-legacy-foto-reproducaodc-comics.jpg'
 const botaoEstaDesabilitado = false;
+
+const gostaDoBatman = false;
+const gostaDoSuperman = true;
 </script>
 
 <template>
   <h1>{{meuObj.filmeFavorito}}</h1>
   <h2>{{dizOi('Ramon') }}</h2> <!--Mustashes-->
-  <img v-bind:src="imgBatman" alt="Imagem do Batman"> <!--Binds-->
+
+  <img v-if="gostaDoBatman":src="imgBatman" alt="Imagem do Batman">
+  <img v-else-if="gostaDoSuperman":src="imgSuperman" alt="Imagem do Superman">
+  <h2 v-else>Não curte herois da DC</h2>
+
   <button :disabled="!botaoEstaDesabilitado">Enviar Mensagem</button>
 </template>
 
 <style scoped>
-
+  img {
+    height: 200px;
+  }
 </style>
