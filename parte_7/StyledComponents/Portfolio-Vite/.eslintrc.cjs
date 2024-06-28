4054d3054f8983eb33c2d61cb2f0583ede89b359
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-06-27 23:09:32 -0300

    reset e config

--- parte_7/StyledComponents/Portfolio-Vite/.eslintrc.cjs
+++ parte_7/StyledComponents/Portfolio-Vite/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
@@ -10,9 +11,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  settings: {
+    'react': {
+      'version': 'detect'
+    }
+  }
 }
