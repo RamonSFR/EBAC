f9b788c564f7437695e08338d3a2a88af37428ca
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-09-21 18:20:09 -0300

    finalizacao testes header

--- parte_8/ebac_games_redux/src/components/__tests__/Header.test.tsx
+++ parte_8/ebac_games_redux/src/components/__tests__/Header.test.tsx
@@ -8,4 +8,35 @@ describe('Testes para o componente header', () => {
     renderizaComProvider(<Header />)
     expect(screen.getByText('EBAC Games')).toBeInTheDocument()
   })
+
+  test('Deve renderizar com dois itens no carrinho', () => {
+    renderizaComProvider(<Header />, {
+      preloadedState: {
+        carrinho: {
+          itens: [
+            {
+              id: 1,
+              titulo: 'Resident Evil 4',
+              plataformas: ['PC', 'PS5', 'Xbox Series S/X'],
+              precoAntigo: 139.99,
+              preco: 99.99,
+              categoria: 'Ação',
+              imagem: ''
+            },
+            {
+              id: 2,
+              titulo: 'Dead Space',
+              plataformas: ['PC', 'PS5', 'Xbox Series S/X'],
+              precoAntigo: 299.99,
+              preco: 69.99,
+              categoria: 'Terror de Sobrevivência',
+              imagem: ''
+            }
+          ]
+        }
+      }
+    })
+
+    expect(screen.getByTestId('qtd-carrinho').innerHTML).toContain('2 itens')
+  })
 })
