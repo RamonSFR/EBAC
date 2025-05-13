30b7fdb97b24e1d3336481fddf827d590f7adfb3
RamonSFR <ramonsaviofontesrocha@gmail.com>
2025-05-13 14:33:41 -0300

    simple app build to test

--- parte_8/react-testing-library/testes-jest/src/App.js
+++ parte_8/react-testing-library/testes-jest/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [tarefas, setTarefas] = useState([])
+  const [tarefaTemp, setTarefaTemp] = useState('')
+
+  function cadastrarTarefa() {
+    setTarefas([...tarefas, tarefaTemp])
+    setTarefaTemp('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={tarefaTemp} onChange={evento => setTarefaTemp(evento.target.value)}></input>
+      <button type="button" onClick={cadastrarTarefa}>cadastrar</button>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li>{tarefa}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
